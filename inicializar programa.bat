65b3b8d5390880e907d8a521e524050a849e2900
Natalia Jung <natbjunghans@gmail.com>
2025-07-17 20:05:18 -0300

    Update bat file
    Adds an initial print and creates the missing test folder

--- inicializar programa.bat
+++ inicializar programa.bat
@@ -3,6 +3,7 @@
 cd /d "%~dp0"
 
 if not exist ".venv" (
+    echo Criando virtual environment...
     python -m venv .venv
 )
 
@@ -11,6 +12,8 @@ call .venv\Scripts\activate.bat
 python -m pip install --upgrade pip
 
 pip install -r requirements.txt
+
+mkdir Teste\Mover_aqui
 
 python Script\main.py
 
